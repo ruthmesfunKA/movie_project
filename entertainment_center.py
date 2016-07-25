00026c23c08d7c3e11a2f1c16609120e638a26b3
ruth mesfun <ruthmesfun@C02RF9QFFVH5.local>
2016-07-25 14:08:19 -0700

    first commit

---
+++ entertainment_center.py
@@ -0,0 +1,70 @@
+import fresh_tomatoes, media
+from lxml import html
+import requests
+
+# Scrapping data from IMDB
+class Imdb():
+	def __init__(self, url, trailer):
+		self.url = url
+		self.trailer = trailer
+
+	def page_tree(self): #  getting data from the HTML 
+		page = requests.get(self.url)
+		tree = html.fromstring(page.content)
+		return tree
+
+	def title(self): # getting the title
+		tree = self.page_tree()
+		title = tree.xpath("//h1[@itemprop='name']/text()")[0]
+		return title
+	
+	def duration(self): # getting the duration from the data
+		tree = self.page_tree()
+		duration = tree.xpath("//time[@itemprop='duration']/text()")[1]
+		return duration
+	
+	def movie_storyline(self): # storyline 
+		tree = self.page_tree()
+		movie_storyline = tree.xpath("//div[@class='summary_text']/text()")[0]
+		return movie_storyline
+
+	def poster_image(self): #  poster image
+		tree = self.page_tree()
+		poster_image = tree.xpath("//img/@src")[2]
+		return poster_image
+	# def title(self.page):
+	# 	title 
+
+galaxy = Imdb('http://www.imdb.com/title/tt2015381', 
+				'https://www.youtube.com/watch?v=89yh3vYs-zo')
+indignation = Imdb('http://www.imdb.com/title/tt4193394',
+					'https://www.youtube.com/watch?v=ELKsrUssyQE')
+
+avatar = Imdb('http://www.imdb.com/title/tt0499549/?ref_=fn_al_tt_1',
+				'https://www.youtube.com/watch?v=d1_JBMrrYw8')
+
+galaxy_site = media.Movie(galaxy.title(), 
+						galaxy.duration(),
+						galaxy.movie_storyline(), 
+						galaxy.poster_image(), 
+						galaxy.trailer)
+
+
+
+indignation_site = media.Movie(galaxy.title(), 
+						indignation.duration(),
+						indignation.movie_storyline(), 
+						indignation.poster_image(), 
+						indignation.trailer)
+
+avatar_site = media.Movie(avatar.title(), 
+						avatar.duration(),
+						avatar.movie_storyline(), 
+						avatar.poster_image(), 
+						avatar.trailer)
+
+
+
+
+movies = [galaxy_site, indignation_site, avatar_site]
+
---
+++ media.py
@@ -0,0 +1,42 @@
+import webbrowser
+
+#Parent class to Movie & TvShow
+class Video():
+    def __init__(self, title,duration):
+        self.title = title
+        self.duration = duration
+
+
+
+
+class Movie(Video):
+    """This class provides a way to store movie related information"""
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    
+    def __init__(self,
+                 title,
+                 duration,
+                 movie_storyline,
+                 poster_image,
+                 trailer_youtube):
+        Video.__init__(self, title, duration)
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+    
+
+class TvShow(Video):
+
+    def __init__(self,
+                 title,
+                 duration,
+                 season,
+                 episode, tv_station):
+        Video.__init__(self,title,duration)
+        self.season = season
+        self.episode = episode
+        self.tv_station = tv_station
+        
